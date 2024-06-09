d23bf565b9e189d217cd4cdebf4ee4750bafb628
duslam22 <mamahga18@gmail.com>
2024-06-09 23:32:48 +0700

    udate sr

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,9 +3,9 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { fetchInvoicesPages } from '@/app/lib/data';
 import { Suspense } from 'react';
- 
+import { fetchInvoicesPages } from '@/app/lib/data';
+
 export default async function Page({
   searchParams,
 }: {
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -16,7 +16,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
-  
+
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
