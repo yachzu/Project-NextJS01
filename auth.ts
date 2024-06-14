75517461feb009d1ee4812e301bad62f9ff98ba3
duslam22 <mamahga18@gmail.com>
2024-06-15 04:44:19 +0700

    update

--- auth.ts
+++ auth.ts
@@ -5,17 +5,17 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
-
-async function getUser(email: string): Promise<User | null> {
+ 
+async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0] ?? null;
+    return user.rows[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
   }
 }
-
+ 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
@@ -24,28 +24,16 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
-
-        if (!parsedCredentials.success) {
-          console.log('Invalid credentials format');
-          return null;
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
         }
-
-        const { email, password } = parsedCredentials.data;
-        const user = await getUser(email);
-
-        if (!user) {
-          console.log('User not found');
-          return null;
-        }
-
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-
-        if (!passwordsMatch) {
-          console.log('Invalid password');
-          return null;
-        }
-
-        return user;
+        console.log('Invalid credentials');
+        return null;
       },
     }),
   ],
